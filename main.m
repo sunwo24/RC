dc56af21f046c0cc23506f6f1f297aac791f6b8a
Huanzhuo Wu <huanzhuo@Huanzhuo.local>
2016-05-27 23:43:36 +0200

    optimal

--- main.m
+++ main.m
@@ -1,5 +1,5 @@
-codeType = 'MSR';
-Parameter = [7 3 5];
+codeType = 'MBR'
+Parameter = [7 3 5]
 
 % Failed node ID
 failedNode = RandFailedNode(Parameter);
@@ -8,72 +8,104 @@ failedNode = RandFailedNode(Parameter);
 [gfExp, U] = BaseGF(codeType, Parameter);
 GF = GaloisField(gfExp);
 
-% Original message
-message = RandMessage(gfExp, U);
+n = 50;
+t = zeros(1, n);
 
 if (strcmp(codeType, 'MBR'))
     % Generator Matrix n * d
     % generatorMatrix = GeneratorMatrixMBR(Parameter, GF);
     generatorMatrix = SysGeneratorMatrixMBR(Parameter, GF);
 
-    % Message Matrix d * d
-    messageMatrix = MessageMatrixMBR(message, Parameter, GF);
+    while n > 0
+        tic;
 
-    % Encode
-    codewordMatrix = generatorMatrix * messageMatrix;
+        % Original message
+        message = RandMessage(gfExp, U);
 
-    % Helper Nodes /randomly
-    Helpers = HelperNodes(Parameter, failedNode);
-    
-    % Regenerating
-    [helperMessage, helperMatrix] = Helper(codewordMatrix,...
-                                    generatorMatrix, failedNode, Helpers, GF);
-    repairedMessage = transpose(helperMatrix \ helperMessage);
+        % Message Matrix d * d
+        messageMatrix = MessageMatrixMBR(message, Parameter, GF);
 
-    % Decode
-    [decodeMatrix, dataCollectorMatrix] = DataCollector(codewordMatrix, generatorMatrix, Parameter);
-    decodedMessageMatrix = DecodeMBR(decodeMatrix, dataCollectorMatrix, Parameter);
-    decodeMessage = GetMessageMBR(decodedMessageMatrix, Parameter, GF);
+        % Encode
+        codewordMatrix = generatorMatrix * messageMatrix;
+
+        % Helper Nodes /randomly
+        Helpers = HelperNodes(Parameter, failedNode);
+        
+        % Regenerating
+        [helperMessage, helperMatrix] = Helper(codewordMatrix,...
+                                        generatorMatrix, failedNode, Helpers, GF);
+        repairedMessage = transpose(helperMatrix \ helperMessage);
+
+        if (isequal(codewordMatrix(failedNode, :), repairedMessage))
+            disp('Regenerating success!');
+        else
+            error('Regenerating fails!');
+        end
+
+        % Decode
+        [decodeMatrix, dataCollectorMatrix] = DataCollector(codewordMatrix, generatorMatrix, Parameter);
+        decodedMessageMatrix = DecodeMBR(decodeMatrix, dataCollectorMatrix, Parameter);
+        decodeMessage = GetMessageMBR(decodedMessageMatrix, Parameter, GF);
+
+        if (isequal(message, decodeMessage))
+            disp('Decoding success!');
+        else
+            error('Decoding fails!');
+        end
+
+        t(n) = toc;
+        n = n - 1;
+    end
 
 elseif (strcmp(codeType, 'MSR'))
     % Generator Matrix n * d
     generatorMatrix = GeneratorMatrixMSR(Parameter, GF);
 
-    % Message Matrix d * d
-    messageMatrix = MessageMatrixMSR(message, Parameter, GF);
+    while n > 0
+        tic;
 
-    % Encode
-    codewordMatrix = generatorMatrix * messageMatrix;
+        % Original message
+        message = RandMessage(gfExp, U);
 
-    % Helper Nodes /randomly
-    Helpers = HelperNodes(Parameter, failedNode);
+        % Message Matrix d * d
+        messageMatrix = MessageMatrixMSR(message, Parameter, GF);
 
-    % Regenerating
-    [helperMessage, helperMatrix] = Helper(codewordMatrix,...
-                                    generatorMatrix, failedNode, Helpers, GF);
-    repairedMessageRe = helperMatrix \ helperMessage;
-    flambda = generatorMatrix(failedNode, 1);
-    repairedMessage = transpose([flambda * (repairedMessageRe(1 : (Parameter(2) - 1), :))...
-                      + (repairedMessageRe(Parameter(2) : (2 * Parameter(2) - 2), :));...
-                      repairedMessageRe((2 * Parameter(2) - 1) : Parameter(3), :)]);
+        % Encode
+        codewordMatrix = generatorMatrix * messageMatrix;
 
-    % Decode
-    [decodeMatrix, dataCollectorMatrix] = DataCollector(codewordMatrix, generatorMatrix, Parameter);
-    decodedMessageMatrix = DecodeMSR(decodeMatrix, dataCollectorMatrix, Parameter);
-    decodeMessage = GetMessageMSR(decodedMessageMatrix, Parameter, GF);
-    
+        % Helper Nodes /randomly
+        Helpers = HelperNodes(Parameter, failedNode);
+
+        % Regenerating
+        [helperMessage, helperMatrix] = Helper(codewordMatrix,...
+                                        generatorMatrix, failedNode, Helpers, GF);
+        repairedMessageRe = helperMatrix \ helperMessage;
+        flambda = generatorMatrix(failedNode, 1);
+        repairedMessage = transpose([flambda * (repairedMessageRe(1 : (Parameter(2) - 1), :))...
+                          + (repairedMessageRe(Parameter(2) : (2 * Parameter(2) - 2), :));...
+                          repairedMessageRe((2 * Parameter(2) - 1) : Parameter(3), :)]);
+
+        if (isequal(codewordMatrix(failedNode, :), repairedMessage))
+            disp('Regenerating success!');
+        else
+            error('Regenerating fails!');
+        end
+
+        % Decode
+        [decodeMatrix, dataCollectorMatrix] = DataCollector(codewordMatrix, generatorMatrix, Parameter);
+        decodedMessageMatrix = DecodeMSR(decodeMatrix, dataCollectorMatrix, Parameter);
+        decodeMessage = GetMessageMSR(decodedMessageMatrix, Parameter, GF);
+
+        if (isequal(message, decodeMessage))
+            disp('Decoding success!');
+        else
+            error('Decoding fails!');
+        end
+        
+        t(n) = toc;
+        n = n - 1;
+    end
 else
     error('wrong code type!!');
 end
-
-if (isequal(codewordMatrix(failedNode, :), repairedMessage))
-    disp('Regenerating success!');
-else
-    error('Regenerating fails!');
-end
-
-if (isequal(message, decodeMessage))
-    disp('Decoding success!');
-else
-    error('Decoding fails!');
-end
+% plot(t)
