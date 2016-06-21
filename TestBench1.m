6ef4c6175e34d19c04fde38d26ba93c57ef8e490
Huanzhuo Wu <huanzhuo@Huanzhuo.local>
2016-06-21 15:21:41 +0200

    testbech optimalpoint

--- TestBench1.m
+++ TestBench1.m
@@ -1,8 +1,9 @@
 % same n, k of MBR and MSR
 % compare disk size, regenerating bandwidth and decoding bandwidth with all possible d.
 
-sameNKMBR = zeros(size(Parameter, 1), 5);
-sameNKMSR = zeros(size(Parameter, 1), 5);
+sameNKMBR = zeros(size(Parameter, 1), 7);
+sameNKMSR = zeros(size(Parameter, 1), 7);
+codeRate = zeros(size(Parameter, 1), 1);
 
 for i = 1 : size(Parameter, 1)
     [MessageSizeMBR, CodedSizeMBR, RegeneratingBandwidthMBR, DecodingBandwidthMBR] = MBR(Parameter(i, :));
@@ -12,14 +13,19 @@ for i = 1 : size(Parameter, 1)
     sameNKMBR(i, 2) = CodedSizeMBR/MessageSizeMBR;
     sameNKMBR(i, 3) = RegeneratingBandwidthMBR/MessageSizeMBR;
     sameNKMBR(i, 4) = DecodingBandwidthMBR/MessageSizeMBR;
-    sameNKMBR(i, 5) = Parameter(i, 1)/Parameter(i, 2);
+    sameNKMBR(i, 5) = Parameter(i, 3)/MessageSizeMBR;
+    sameNKMBR(i, 6) = CodedSizeMBR;
+    sameNKMBR(i, 7) = RegeneratingBandwidthMBR;
 
     sameNKMSR(i, 1) = MessageSizeMSR/MessageSizeMSR;
     sameNKMSR(i, 2) = CodedSizeMSR/MessageSizeMSR;
     sameNKMSR(i, 3) = RegeneratingBandwidthMSR/MessageSizeMSR;
     sameNKMSR(i, 4) = DecodingBandwidthMSR/MessageSizeMSR;
-    sameNKMSR(i, 5) = 1/Parameter(i, 2);
+    sameNKMSR(i, 5) = Parameter(i, 3)/MessageSizeMSR;
+    sameNKMSR(i, 6) = CodedSizeMSR;
+    sameNKMSR(i, 7) = RegeneratingBandwidthMSR;
 
+    codeRate(i, 1) = Parameter(i, 1)/Parameter(i, 2);
 end
 
 x = transpose(Parameter(:, 3));
@@ -27,33 +33,44 @@ percMessageSizeMBR = sameNKMBR(:, 1);
 percCodedSizeMBR = sameNKMBR(:, 2);
 percRegeneratingBandwidthMBR = sameNKMBR(:, 3);
 percDecodingBandwidthMBR = sameNKMBR(:, 4);
-codeRate = sameNKMBR(:, 5);
+minRegeneratingBandwidthMBR = sameNKMBR(:, 5);
+CodedSizeMBR = sameNKMBR(:, 6);
+RegeneratingBandwidthMBR = sameNKMBR(:, 7);
 
 percMessageSizeMSR = sameNKMSR(:, 1);
 percCodedSizeMSR = sameNKMSR(:, 2);
 percRegeneratingBandwidthMSR = sameNKMSR(:, 3);
 percDecodingBandwidthMSR = sameNKMSR(:, 4);
-minRegeneratingBandwidth = sameNKMSR(:, 5);
+minRegeneratingBandwidthMSR = sameNKMSR(:, 5);
+CodedSizeMSR = sameNKMSR(:, 6);
+RegeneratingBandwidthMSR = sameNKMSR(:, 7);
 
 figure
-s(1) = subplot(3, 1, 1);
+s(1) = subplot(4, 1, 1);
 plot(x, percCodedSizeMBR, '--r+', x, percCodedSizeMSR, '--b+', x, codeRate, ':ko')
 axis([Parameter(1, 3) - 1, Parameter(end, 3) + 1, (codeRate(1, 1) - 0.3), (percCodedSizeMBR(1, 1) + 0.3)])
 legend('E-MBR', 'E-MSR', 'Koderate')
 xlabel('d');
 ylabel('Speicher %');
 
-s(2) = subplot(3, 1, 2);
-plot(x, percRegeneratingBandwidthMBR, '--r+', x, percRegeneratingBandwidthMSR, '--b+', x, minRegeneratingBandwidth, ':kd')
-axis([Parameter(1, 3) - 1, Parameter(end, 3) + 1, (minRegeneratingBandwidth(end) - 0.05), (percRegeneratingBandwidthMSR(1) + 0.05)])
-legend('SR E-MBR', 'SR E-MSR', 'theoretische Bandbreite')
+s(2) = subplot(4, 1, 2);
+plot(x, percRegeneratingBandwidthMBR, '--r+', x, percRegeneratingBandwidthMSR, '--b+', x, minRegeneratingBandwidthMBR, ':rd', x, minRegeneratingBandwidthMSR, ':bd')
+axis([Parameter(1, 3) - 1, Parameter(end, 3) + 1, (percRegeneratingBandwidthMBR(end) - 0.05), (percRegeneratingBandwidthMSR(1) + 0.05)])
+legend('SR E-MBR', 'SR E-MSR', 'ges. Bandbreite MBR', 'ges. Bandbreite MSR')
 xlabel('d');
-ylabel('Bandbreite %');
+ylabel('Reparatur-Bandbreite %');
 
-s(3) = subplot(3, 1, 3);
+s(3) = subplot(4, 1, 3);
+plot(x, RegeneratingBandwidthMBR, '--r+', x, RegeneratingBandwidthMSR, '--bo')
+axis([Parameter(1, 3) - 1, Parameter(end, 3) + 1, (RegeneratingBandwidthMBR(1, 1) - 3), (RegeneratingBandwidthMBR(end, 1) + 3)])
+legend('SR E-MBR', 'SR E-MSR')
+xlabel('d');
+ylabel('Reparatur-Bandbreite');
+
+s(4) = subplot(4, 1, 4);
 plot(x, percDecodingBandwidthMBR, '--r+', x, percDecodingBandwidthMSR, '--b+', x, percMessageSizeMBR, ':ks')
 axis([Parameter(1, 3) - 1, Parameter(end, 3) + 1, 0.9, (percDecodingBandwidthMBR(1, 1) + 0.1)])
 legend('E-MBR', 'E-MSR', 'Datenobjekt')
 xlabel('d');
-ylabel('Bandbreite %');
+ylabel('Dekodierung-Bandbreite %');
 
--- TestBench3.m
+++ TestBench3.m
@@ -2,7 +2,7 @@
 % get the optimal point of parallel repaire
 
 optimalPoint = zeros(size(Parameter, 1), 2);
-availNumNodes = zeros(size(Parameter, 1), 1);
+availNumNodes = zeros(size(Parameter, 1), 2);
 
 for i = 1 : size(Parameter, 1)
     [optimalPointMBR, optimalPointMSR] = OptimalPoint(Parameter(i, :));
@@ -11,14 +11,17 @@ for i = 1 : size(Parameter, 1)
     optimalPoint(i, 2) = optimalPointMSR;
 
     availNumNodes(i, 1) = Parameter(i, 1) - Parameter(i, 3);
+    availNumNodes(i, 2) = Parameter(i, 1) - Parameter(i, 2);
 end
 
 x = Parameter(:, 3);
 optimalPointMBR = optimalPoint(:, 1);
 optimalPointMSR = optimalPoint(:, 2);
+availNumNodesPR = availNumNodes(:, 1);
+availNumNodesMDSR = availNumNodes(:, 2);
 
-plot(x, optimalPointMBR, '--r*', x, optimalPointMSR, '--b+', x, availNumNodes, '--ko')
-axis([Parameter(1, 3) - 0.5, Parameter(end, 3) + 0.5, (optimalPointMSR(end, 1) - 1), (availNumNodes(1, 1) + 1)])
-legend('E-MBR', 'E-MSR', 'max. ausgefallene Knoten')
+plot(x, optimalPointMBR, '--r*', x, optimalPointMSR, '--b+', x, availNumNodesPR, '--ko', x, availNumNodesMDSR, '--ks')
+axis([Parameter(1, 3) - 0.5, Parameter(end, 3) + 0.5, (optimalPointMSR(end, 1) - 1), (availNumNodes(1, 2) + 3)])
+legend('E-MBR', 'E-MSR', 'max. PR |f|', 'max. MDSR |f|')
 xlabel('d');
 ylabel('Optimalpunkt');
